c38329de243bff11186ca2c4f7061c58c847e042
ulloaDL <46727928+ulloadl@users.noreply.github.com>
2020-11-18 15:06:01 -0600

    Symbol class

--- mini_parser/model/symbol.py
+++ mini_parser/model/symbol.py
@@ -1,5 +1,34 @@
-class Symbol:
 
-    def __init__(self):
-        pass
+
+class Symbol:
+    type = " "
+    name = " "
+    value = object
+    line = int
+    field = " "
+
+    def __init__(self, type_, name_, value_, line_, field_):
+        self.type = type_
+        self.name = name_
+        self.value = value_
+        self.line = line_
+        self.field = field_
+
+    def get_type(self):
+        return self.type
+
+    def get_name(self):
+        return self.name
+
+    def get_value(self):
+        return  self.value
+
+    def get_line(self):
+        return self.line
+
+    def get_field(self):
+        return self.field
+
+    def __str__(self):
+        return self.type + "/" + self.name + "/" + str(self.value) + "/" + str(self.line) + "/" + self.field
 
