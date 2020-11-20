0e94907bb9381caf7df1071d0353e7b708ff43a2
Miguel-SS <55169938+Miguel-SS@users.noreply.github.com>
2020-11-20 02:16:25 -0600

    count on lines done

--- mini_parser/mini_parser.py
+++ mini_parser/mini_parser.py
@@ -3,22 +3,23 @@ from .model.symbol import Symbol
 
 scopes_stack = []
 errors_list = []
-# line_counter = 0
+line_counter = 0
 
 
 def parse_declaration(tokens: list):
     name = tokens[1][0]
-    value = Symbol(tokens[0][0], name, None, 1, scopes_stack[-1])
+    value = Symbol(tokens[0][0], name, None, line_counter, scopes_stack[-1])
     SymbolTable.add_symbol(name, value)
 
 
 def parse_assignment(tokens: list):
     name = tokens[0][0]
-    value = Symbol(tokens[2][1], name, tokens[2][0], 1, scopes_stack[-1])
+    value = Symbol(tokens[2][1], name, tokens[2][0], line_counter, scopes_stack[-1])
     if SymbolTable.find_symbol(name).get_type() == value.get_type():
         SymbolTable.add_symbol(name, value)
     else:
-        error = 'Error linea x: "' + name + '" parametro incorrecto - se esperaba (' + value.get_type() + ')'
+        error = 'Error linea ' + str(line_counter) + ': "' + name + '" parametro incorrecto - se esperaba (' + \
+                value.get_type() + ')'
         errors_list.append(error)
         # print('Error linea x: "' + name + '" parametro incorrecto - se esperaba (' + value.get_type() + ')')
 
@@ -27,20 +28,20 @@ def parse_assign_declaration(tokens: list):
     name = tokens[1][0]
     val_symbol = SymbolTable.find_symbol(tokens[3][0])
     if val_symbol is not None:
-        value = Symbol(val_symbol.get_type(), name, val_symbol.get_value(), 1, scopes_stack[-1])
+        value = Symbol(val_symbol.get_type(), name, val_symbol.get_value(), line_counter, scopes_stack[-1])
         if tokens[0][0] == value.get_type():
             SymbolTable.add_symbol(name, value)
         else:
-            error = 'Error linea x: Asignacion incorrecta ' + val_symbol.get_name() + ' (' + val_symbol.get_type() + \
+            error = 'Error linea ' + str(line_counter) + ': Asignacion incorrecta ' + val_symbol.get_name() + ' (' + val_symbol.get_type() + \
                     ') a ' + name + ' (' + tokens[0][0] + ')'
             errors_list.append(error)
             # print('Error linea x: Asignacion incorrecta ' + val_symbol.get_name() + ' (' + val_symbol.get_type() + ') a ' + name + ' (' + tokens[0][0] + ')')
     else:
-        value = Symbol(tokens[3][1], name, tokens[3][0], 1, scopes_stack[-1])
+        value = Symbol(tokens[3][1], name, tokens[3][0], line_counter, scopes_stack[-1])
         if tokens[0][0] == value.get_type():
             SymbolTable.add_symbol(name, value)
         else:
-            error = 'Error linea x: Asignacion incorrecta ' + tokens[3][0] + ' (' + tokens[3][1] + ') a' + name + ' (' \
+            error = 'Error linea ' + str(line_counter) + ': Asignacion incorrecta ' + tokens[3][0] + ' (' + tokens[3][1] + ') a' + name + ' (' \
                     + tokens[0][0] + ')'
             errors_list.append(error)
             # print('Error linea x: Asignacion incorrecta ' + tokens[3][0] + ' (' + tokens[3][1] + ') a' + name + ' (' + tokens[0][0] + ')')
@@ -57,7 +58,7 @@ def parse_function(tokens: list):
         i = i+2
 
     name = tokens[1][0]
-    value = Symbol(tokens[0][0], name, None, 1, 'global')
+    value = Symbol(tokens[0][0], name, None, line_counter, 'global')
     SymbolTable.add_symbol(name, value)
 
 
@@ -68,20 +69,20 @@ def parse_return(tokens: list):
         if value.get_type() == val_function.get_type():
             val_function.set_value(value.get_value)
         else:
-            error = 'Valor de retorno (' + value.get_type() + ') no coincide con el valor de retorno de la funcion (' \
+            error = 'Error linea ' + str(line_counter) + ': Valor de retorno (' + value.get_type() + \
+                    ') no coincide con el valor de retorno de la funcion (' \
                     + val_function.get_type() + ')'
             errors_list.append(error)
-            # print('Valor de retorno (' + value.get_type() + ') no coincide con el valor de retorno de la funcion (' + val_function.get_type() + ')')
 
     elif (tokens[1][1] == 'string') or (tokens[1][1] == 'float') or (tokens[1][1] == 'int'):
         val_function = SymbolTable.find_symbol(scopes_stack[-1][6:])
         if tokens[1][1] == val_function.get_type():
             val_function.set_value(tokens[1][0])
         else:
-            error = 'Valor de retorno (' + tokens[1][1] + ') no coincide con el valor de retorno de la funcion (' + \
+            error = 'Error linea ' + str(line_counter) + ': Valor de retorno (' + tokens[1][1] + \
+                    ') no coincide con el valor de retorno de la funcion (' + \
                     val_function.get_type() + ')'
             errors_list.append(error)
-            # print('Valor de retorno (' + tokens[1][1] + ') no coincide con el valor de retorno de la funcion (' + val_function.get_type() + ')')
 
 
 def parse_conditional(tokens: list):
@@ -100,13 +101,11 @@ def parse_conditional(tokens: list):
         val_2 = tokens[4][1]
 
     if val_1 != val_2:
-        error = 'Error en la linea x: Comparacion de (' + val_1 + ') con (' + val_2 + ')'
+        error = 'Error en la linea ' + str(line_counter) + ': Comparacion de (' + val_1 + ') con (' + val_2 + ')'
         errors_list.append(error)
-        # print('Error en la linea x: Comparacion de (' + val_1 + ') con (' + val_2 + ')')
     if tokens[3][1] != 'equals':
-        error = 'Error en la linea x: Signo incorrecto, no es de comparacion'
+        error = 'Error en la linea ' + str(line_counter) + ': Signo incorrecto, no es de comparacion'
         errors_list.append(error)
-        # print('Error en la linea x: Signo incorrecto, no es de comparacion')
 
 
 def call_function(tokens: list):
@@ -141,8 +140,9 @@ def line_parser(tokens: list):
 
 def mini_parser(lines: list):
     scopes_stack.append('global')
+    global line_counter
     for line in lines:
-        # line_counter = line_counter + 1
+        line_counter = line_counter + 1
         if line[0][1] != 'empty':
             line_parser(line)
 
